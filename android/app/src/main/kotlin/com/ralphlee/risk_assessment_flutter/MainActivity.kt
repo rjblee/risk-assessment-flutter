7c2a6a1dba53fd88ff42c5eed9c7f08bb6dcd109
Ralph Lee <ralph.lee@live.ca>
2021-03-02 16:13:37 +0900

    Initial project setup

---
+++ android/app/src/main/kotlin/com/ralphlee/risk_assessment_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ralphlee.risk_assessment_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SAHARA',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: OpenApp(),
+    );
+  }
+}
+
+class OpenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Safety in Schools'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              'SAHARA',
+            ),
+            Text(
+              'Safety & Hazard Risk Assessment App',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            FlatButton(
+              color: Colors.red,
+              onPressed: () {},
+              child: Text('Enter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
