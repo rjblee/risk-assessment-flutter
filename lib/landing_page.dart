338c6f185ca4133c18190907db6d42fb04000ddf
Ralph Lee <ralph.lee@live.ca>
2021-03-03 14:59:57 +0900

    Edit file and class names with new names

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'landing_page.dart';
 
-class LandingPage extends StatelessWidget {
+class OpenApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,63 +13,54 @@ class LandingPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Text(
-                  'ABOUT THE APP',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
-                  style: TextStyle(
-                    fontSize: 16,
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    'SAHARA',
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Text(
-                  'TERMS AND CONDITIONS',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.',
-                  style: TextStyle(
-                    fontSize: 16,
+                  Text(
+                    'Safety & Hazard Risk Assessment App',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
+                ],
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 60),
+                child: Image.asset(
+                  'images/SIS Logo.png',
+                  width: 300,
                 ),
-              ],
+              ),
             ),
-          ),
-          FlatButton(
-            padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
+            FlatButton(
+              padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              color: Color(0XFFB5121B),
+              textColor: Color(0xFFFFFFFF),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return LandingPage();
+                }));
+              },
+              child: Text(
+                'Enter',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-            color: Color(0XFFB5121B),
-            textColor: Color(0xFFFFFFFF),
-            onPressed: () {
-              // Navigator.push(context, MaterialPageRoute(builder: (context) {
-              //   return LocationScreen(
-              //     locationWeather: weatherData,
-              //   );
-              // }));
-            },
-            child: Text(
-              'Click to Accept',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
