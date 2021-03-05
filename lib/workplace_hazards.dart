0c44a0fdf56e96a449c95d10f705106c956b5db7
Ralph Lee <ralph.lee@live.ca>
2021-03-06 00:49:02 +0900

    Create workplace hazards page with dropdown menus

--- lib/workplace_hazards.dart
+++ lib/workplace_hazards.dart
@@ -1,5 +1,6 @@
 import 'package:multi_select_flutter/multi_select_flutter.dart';
 import 'package:flutter/material.dart';
+import 'appbar.dart';
 
 class WorkplaceHazards extends StatefulWidget {
   @override
@@ -7,67 +8,56 @@ class WorkplaceHazards extends StatefulWidget {
 }
 
 class _WorkplaceHazardsState extends State<WorkplaceHazards> {
-  static List<Animal> _animals = [
-    Animal(id: 1, name: "Lion"),
-    Animal(id: 2, name: "Flamingo"),
-    Animal(id: 3, name: "Hippo"),
-    Animal(id: 4, name: "Horse"),
-    Animal(id: 5, name: "Tiger"),
-    Animal(id: 6, name: "Penguin"),
-    Animal(id: 7, name: "Spider"),
-    Animal(id: 8, name: "Snake"),
-    Animal(id: 9, name: "Bear"),
-    Animal(id: 10, name: "Beaver"),
-    Animal(id: 11, name: "Cat"),
-    Animal(id: 12, name: "Fish"),
-    Animal(id: 13, name: "Rabbit"),
-    Animal(id: 14, name: "Mouse"),
-    Animal(id: 15, name: "Dog"),
-    Animal(id: 16, name: "Zebra"),
-    Animal(id: 17, name: "Cow"),
-    Animal(id: 18, name: "Frog"),
-    Animal(id: 19, name: "Blue Jay"),
-    Animal(id: 20, name: "Moose"),
-    Animal(id: 21, name: "Gecko"),
-    Animal(id: 22, name: "Kangaroo"),
-    Animal(id: 23, name: "Shark"),
-    Animal(id: 24, name: "Crocodile"),
-    Animal(id: 25, name: "Owl"),
-    Animal(id: 26, name: "Dragonfly"),
-    Animal(id: 27, name: "Dolphin"),
+  static List<Hazard> _hazards = [
+    Hazard(id: 1, name: "Body stressing"),
+    Hazard(id: 2, name: "Confined spaces"),
+    Hazard(id: 3, name: "Electricity"),
+    Hazard(id: 4, name: "Heat"),
+    Hazard(id: 5, name: "Cold"),
+    Hazard(id: 6, name: "Noise"),
+    Hazard(id: 7, name: "Vibration"),
+    Hazard(id: 8, name: "Temperature extremes"),
+    Hazard(id: 9, name: "Radiation"),
+    Hazard(id: 10, name: "Heights"),
+    Hazard(id: 11, name: "Viruses"),
+    Hazard(id: 12, name: "Toxins"),
+    Hazard(id: 13, name: "Spores"),
+    Hazard(id: 14, name: "Fungi"),
+    Hazard(id: 15, name: "Pathogenic micro-organisms"),
+    Hazard(id: 16, name: "Bio-active substances"),
+    Hazard(id: 17, name: "Bacteria"),
+    Hazard(id: 18, name: "Acids"),
+    Hazard(id: 19, name: "Caustic substances"),
+    Hazard(id: 20, name: "Cleaning products"),
+    Hazard(id: 21, name: "Glues"),
+    Hazard(id: 22, name: "Pesticides"),
+    Hazard(id: 23, name: "Petroleum products"),
+    Hazard(id: 24, name: "Mercury"),
+    Hazard(id: 25, name: "Explosive"),
+    Hazard(id: 26, name: "Flammable"),
+    Hazard(id: 27, name: "Corrosive"),
   ];
 
-  final _items = _animals.map((animal) => MultiSelectItem<Animal>(animal, animal.name)).toList();
+  final _items = _hazards.map((hazard) => MultiSelectItem<Hazard>(hazard, hazard.name)).toList();
 
-  List<Animal> _selectedAnimals = [];
-
-  List<Animal> _selectedAnimals2 = [];
-
-  List<Animal> _selectedAnimals3 = [];
-
-  List<Animal> _selectedAnimals4 = [];
-
-  List<Animal> _selectedAnimals5 = [];
+  List<Hazard> _selectedHazards = [];
+  List<Hazard> _selectedHazards2 = [];
+  List<Hazard> _selectedHazards3 = [];
+  List<Hazard> _selectedHazards4 = [];
+  List<Hazard> _selectedHazards5 = [];
 
   final _multiSelectKey = GlobalKey<FormFieldState>();
 
   @override
   void initState() {
-    _selectedAnimals5 = _animals;
+    _selectedHazards5 = _hazards;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Image.asset(
-            'images/SIS Logo.png',
-            width: 240,
-          ),
-        ),
-      ),
+      appBar: myAppBar(),
       body: Container(
         child: Center(
           child: Column(
@@ -85,34 +75,15 @@ class _WorkplaceHazardsState extends State<WorkplaceHazards> {
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(18.0),
-                    child: MultiSelectDialogField(
-                      items: _items,
-                      title: Text("Animals"),
-                      selectedColor: Colors.red,
-                      selectedItemsTextStyle: TextStyle(fontSize: 20),
-                      decoration: BoxDecoration(
-                        color: Colors.blue.withOpacity(0.1),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        border: Border.all(
-                          color: Colors.blue,
-                          width: 2,
-                        ),
-                      ),
-                      buttonIcon: Icon(
-                        Icons.pets,
-                        color: Colors.blue,
-                      ),
-                      buttonText: Text(
-                        "Favorite Animals",
-                        style: TextStyle(
-                          color: Colors.blue[800],
-                          fontSize: 24,
-                        ),
-                      ),
-                      onConfirm: (results) {
-                        _selectedAnimals = results;
-                      },
-                    ),
+                    child: HazardDropdown('Physical Hazards'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: HazardDropdown('Biological Hazards'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: HazardDropdown('Chemical Hazards'),
                   ),
                 ],
               ),
@@ -122,43 +93,47 @@ class _WorkplaceHazardsState extends State<WorkplaceHazards> {
       ),
     );
   }
+
+  // Extracted Hazard Dropdown widget
+  MultiSelectDialogField<Hazard> HazardDropdown(hazardType) {
+    return MultiSelectDialogField(
+      items: _items,
+      title: Text("$hazardType"),
+      selectedColor: Colors.green,
+      selectedItemsTextStyle: TextStyle(fontSize: 20),
+      decoration: BoxDecoration(
+        color: Color(0xFF2f7edb).withOpacity(0.7),
+        borderRadius: BorderRadius.all(Radius.circular(14)),
+        // border: Border.all(
+        //   color: Color(0xFF2f7edb),
+        //   width: 2,
+        // ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.6),
+            spreadRadius: 3,
+            blurRadius: 6,
+            offset: Offset(0, 5), // changes position of shadow
+          ),
+        ],
+      ),
+      buttonText: Text(
+        "$hazardType",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
+      ),
+      onConfirm: (results) {
+        _selectedHazards = results;
+      },
+    );
+  }
 }
 
-// class EnvironmentBox extends StatelessWidget {
-//   EnvironmentBox(this.environmentName);
-//
-//   final String environmentName;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-//       child: RaisedButton(
-//         padding: EdgeInsets.symmetric(vertical: 30),
-//         shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.circular(20.0),
-//         ),
-//         // color: Color(0XFF3F51B5),
-//         color: Colors.green[200],
-//         // textColor: Color(0xFFFFFFFF),
-//         // splashColor: Colors.grey,
-//         elevation: 10,
-//         onPressed: () {},
-//         child: Text(
-//           environmentName,
-//           style: TextStyle(fontSize: 20),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class Hazard {
+  Hazard({this.id, this.name});
 
-class Animal {
   final int id;
   final String name;
-
-  Animal({
-    this.id,
-    this.name,
-  });
 }
