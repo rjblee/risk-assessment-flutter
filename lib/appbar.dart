ddd128e8d262931767a1d02b6c94fb74ef90390d
Ralph Lee <ralph.lee@live.ca>
2021-03-07 01:17:42 +0900

    Create a separate component for appbar called myAppBar

---
+++ lib/appbar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+AppBar myAppBar() {
+  return AppBar(
+    title: Center(
+      child: Image.asset(
+        'images/SIS Logo.png',
+        width: 240,
+      ),
+    ),
+  );
+}
