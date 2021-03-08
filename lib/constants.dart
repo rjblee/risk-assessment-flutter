d1e7c8b408624cd671f8f04f7a9b115b85204c77
Ralph Lee <ralph.lee@live.ca>
2021-03-09 01:08:44 +0900

    Add slider number style

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Colours
+const kSISColour = Color(0XFFB5121B);
+
+// Text Styles
+const kHeaderTextStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+const kSubHeaderTextStyle = TextStyle(fontSize: 16);
+const kSliderNumberStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
