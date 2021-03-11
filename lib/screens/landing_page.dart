c0fa2a215d5bf2e1abfe2c8df19731c14cc7df73
Ralph Lee <ralph.lee@live.ca>
2021-03-12 01:39:43 +0900

    Change Sahara logo

---
+++ lib/screens/landing_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:risk_assessment_flutter/constants.dart';
+import 'package:risk_assessment_flutter/next_button.dart';
+import '../appbar.dart';
+import 'agreement.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: [
+                Image.asset(
+                  'images/SAHARA Placeholder Square.png',
+                  // width: 380,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 50),
+            child: Column(
+              children: [
+                Text(
+                  'Powered by',
+                  style: kSubHeaderTextStyle,
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: Image.asset(
+                    'images/SIS Logo.png',
+                    width: 240,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          NextButton(
+            buttonText: 'Get Started',
+            nextWidget: Agreement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
