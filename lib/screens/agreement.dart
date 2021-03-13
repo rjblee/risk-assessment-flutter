078ba0f92e0959b400a092903f7ce470d79f147e
Ralph Lee <ralph.lee@live.ca>
2021-03-14 00:46:36 +0900

    Edit text style and button style

--- lib/screens/agreement.dart
+++ lib/screens/agreement.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:risk_assessment_flutter/constants.dart';
+import 'package:risk_assessment_flutter/next_button.dart';
 import 'sign_in.dart';
+import 'package:risk_assessment_flutter/appbar.dart';
 
 class Agreement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Image.asset(
-            'images/SIS Logo.png',
-            width: 240,
-          ),
-        ),
-      ),
+      appBar: myAppBar(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
@@ -23,14 +19,11 @@ class Agreement extends StatelessWidget {
               children: [
                 Text(
                   'ABOUT THE APP',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'YanoneKaffeesatz'),
+                  style: kHeaderTextStyle,
                 ),
                 Text(
                   'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
-                  style: TextStyle(
-                    fontFamily: 'TitilliumWeb',
-                    fontSize: 16,
-                  ),
+                  style: kBodyTextStyle,
                 ),
               ],
             ),
@@ -42,34 +35,18 @@ class Agreement extends StatelessWidget {
               children: [
                 Text(
                   'TERMS AND CONDITIONS',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'YanoneKaffeesatz'),
+                  style: kHeaderTextStyle,
                 ),
                 Text(
                   'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.',
-                  style: TextStyle(
-                    fontFamily: 'TitilliumWeb',
-                    fontSize: 16,
-                  ),
+                  style: kBodyTextStyle,
                 ),
               ],
             ),
           ),
-          FlatButton(
-            padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25.0),
-            ),
-            color: Color(0XFFB5121B),
-            textColor: Color(0xFFFFFFFF),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return SignIn();
-              }));
-            },
-            child: Text(
-              'Click to Accept',
-              style: TextStyle(fontSize: 20),
-            ),
+          NextButton(
+            buttonText: 'Click to Accept',
+            nextWidget: SignIn(),
           ),
         ],
       ),
