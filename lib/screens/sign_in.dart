e9c56e74abb06a6731248581963000d396e5bda3
Ralph Lee <ralph.lee@live.ca>
2021-03-14 00:48:21 +0900

    Edit button layout

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:risk_assessment_flutter/appbar.dart';
-import 'dart:io';
+import 'package:risk_assessment_flutter/constants.dart';
+import 'package:risk_assessment_flutter/next_button.dart';
 import 'industry.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'signup_browser.dart';
+import '../signup_browser.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -16,103 +15,91 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar(),
-      body: ListView(
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20),
-            // decoration: BoxDecoration(
-            //   border: Border(
-            //     top: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-            //     left: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-            //     right: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
-            //     bottom: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
-            //   ),
-            //   color: Color(0xFFBFBFBF),
-            // ),
-            child: Text(
-              'Sign in with your \nSafety in Schools Credentials',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            width: 200,
-            child: Image.asset('images/Swift Learning Logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(80, 10, 80, 10),
-            child: Column(
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter Your email',
-                    labelText: 'Email',
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'Sign in with your Safety in Schools Credentials',
+                    textAlign: TextAlign.center,
+                    style: kSubHeaderTextStyle,
                   ),
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter Your password',
-                    labelText: 'Password',
+                Container(
+                  child: Image.asset('images/Swift Learning Logo.png', height: 120),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'This is the same account you use to \naccess your courses on Swift Learning.',
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(80, 10, 80, 10),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter Your email',
+                          labelText: 'Email',
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter Your password',
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                // NextButton(
+                //   buttonText: 'Log In',
+                //   nextWidget: Industry(),
+                // ),
+                Container(
+                  width: 300,
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: ElevatedButton(
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(fontSize: 20, fontFamily: 'YanoneKaffeesatz'),
+                    ),
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        return Industry();
+                      }));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: kAppBlue,
+                      onPrimary: kAppLight,
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      // padding: EdgeInsets.fromLTRB(80, 15, 80, 15),
+                      padding: EdgeInsets.symmetric(vertical: 14),
+                    ),
+                  ),
+                ),
+                NextButton(
+                  buttonText: 'Create an Account',
+                  nextWidget: SignUpBrowser(),
                 ),
               ],
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              'This is the same account you use to access \nyour courses on Swift Learning.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              RaisedButton(
-                child: Text(
-                  "Sign Up",
-                  style: TextStyle(fontSize: 20),
-                ),
-                onPressed: () {
-                  // launchURL('https://swiftlearning.com/#!/signIn');
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return SignUpBrowser();
-                  }));
-                },
-                color: Colors.blue[800],
-                textColor: Colors.white,
-                padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                splashColor: Colors.blueGrey,
-                elevation: 10,
-              ),
-              RaisedButton(
-                padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                color: Color(0XFFB5121B),
-                textColor: Color(0xFFFFFFFF),
-                elevation: 10,
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Industry();
-                  }));
-                },
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -120,42 +107,10 @@ class _SignInState extends State<SignIn> {
 
 // ----- URL launching Flutter package -----
 
-void launchURL(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
-  }
-}
-
-// ----- Webview Flutter package -----
-
-// class CustomWebView extends StatefulWidget {
-//   CustomWebView(this.url);
-//
-//   final url;
-//
-//   @override
-//   _WebViewState createState() => _WebViewState(this.url);
-// }
-//
-// class _WebViewState extends State<WebView> {
-//   _WebViewState(this.url);
-//
-//   String _url;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Expanded(
-//       child: WebView(
-//         initialUrl: _url,
-//       ),
-//     );
+// void launchURL(String url) async {
+//   if (await canLaunch(url)) {
+//     await launch(url);
+//   } else {
+//     throw 'Could not launch $url';
 //   }
 // }
