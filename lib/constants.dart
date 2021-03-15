6c21cbc5f62587c0495024a1ef71ca5fe4d517a3
Ralph Lee <ralph.lee@live.ca>
2021-03-15 16:14:00 +0900

    Edit fonts in the kBodyTextStyle

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 
 // Colours
 const kSISColour = Color(0XFFB5121B);
+const kAppDark = Color(0XFF0C0F1E);
+const kAppLight = Color(0XFFDCD6DA);
+const kAppBlue = Color(0XFF08A1FA);
+const kAppGreen = Color(0XFFD7FF61);
+const kSISRed = Color(0XFFC40212);
+const kSISBrown = Color(0XFF523C30);
+
+const kResultLow = Color(0XFFb6ff5c);
+const kResultMedium = Color(0XFFffbe33);
+const kResultHigh = Color(0XFFe43a48);
 
 // Text Styles
-const kHeaderTextStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
-const kSubHeaderTextStyle = TextStyle(fontSize: 16);
-const kSliderNumberStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+const kHeaderTextStyle = TextStyle(fontSize: 26, fontWeight: FontWeight.bold, fontFamily: 'RockNRollOne');
+const kSubHeaderTextStyle = TextStyle(fontSize: 22, letterSpacing: 0.6, fontFamily: 'YanoneKaffeesatz');
+const kBodyTextStyle = TextStyle(fontFamily: 'YanoneKaffeesatz');
+const kSliderNumberStyle = TextStyle(fontSize: 40, fontWeight: FontWeight.bold, fontFamily: 'TitilliumWebRegular');
+
+// Button Styles
+// const kNextButtonStyle =
