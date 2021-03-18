4bbdedb36e1c49bba2f5b7e4ba28ed8d5981dace
Ralph Lee <ralph.lee@live.ca>
2021-03-19 00:08:53 +0900

    Create a separate file for hazard container

---
+++ lib/hazard_container.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'workplace_hazards.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class  extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
