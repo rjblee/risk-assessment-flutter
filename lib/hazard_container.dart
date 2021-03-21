af726800a2e60a8c524c5620a7b8f36b92a5b122
Ralph Lee <ralph.lee@live.ca>
2021-03-21 17:36:46 +0900

    Create a separate file for hazard container component

--- lib/hazard_container.dart
+++ lib/hazard_container.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
-import 'workplace_hazards.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:risk_assessment_flutter/screens/workplace_hazards.dart';
 
-
-class  extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+//
+// class HazardContainer extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Column(
+//         children: [
+//           StreamBuilder<QuerySnapshot>(
+//             stream: widget.workplaceHazard,
+//             builder: (context, snapshot) {
+//               if (snapshot.hasData) {
+//                 final hazardCategories = snapshot.data.docs;
+//                 // print(environments);
+//                 final List<Widget> hazardCategoryList = [];
+//
+//                 for (var hazardCategory in hazardCategories) {
+//                   print(hazardCategory['hazard_category_name']);
+//                   hazardCategoryList.add(HazardDropdown(hazardCategory['hazard_category_name']));
+//                 }
+//
+//                 return Column(
+//                   children: hazardCategoryList,
+//                 );
+//               } else {
+//                 // return Text('Snapshot Error');
+//                 return Center(
+//                   child: CircularProgressIndicator(),
+//                 );
+//               }
+//             },
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
