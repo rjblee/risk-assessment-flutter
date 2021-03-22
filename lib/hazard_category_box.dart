bf07446b837a496a963b82531985c3cfa12da469
Ralph Lee <ralph.lee@live.ca>
2021-03-22 21:16:52 +0900

    Create HazardCategoryBox constructor

--- lib/hazard_category_box.dart
+++ lib/hazard_category_box.dart
@@ -1,40 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:risk_assessment_flutter/screens/workplace_hazards.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:risk_assessment_flutter/screens/hazard_category.dart';
 
-//
-// class HazardContainer extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: Column(
-//         children: [
-//           StreamBuilder<QuerySnapshot>(
-//             stream: widget.workplaceHazard,
-//             builder: (context, snapshot) {
-//               if (snapshot.hasData) {
-//                 final hazardCategories = snapshot.data.docs;
-//                 // print(environments);
-//                 final List<Widget> hazardCategoryList = [];
-//
-//                 for (var hazardCategory in hazardCategories) {
-//                   print(hazardCategory['hazard_category_name']);
-//                   hazardCategoryList.add(HazardDropdown(hazardCategory['hazard_category_name']));
-//                 }
-//
-//                 return Column(
-//                   children: hazardCategoryList,
-//                 );
-//               } else {
-//                 // return Text('Snapshot Error');
-//                 return Center(
-//                   child: CircularProgressIndicator(),
-//                 );
-//               }
-//             },
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
+class HazardCategoryBox extends StatelessWidget {
+  HazardCategoryBox({this.hazardCategoryName, this.hazardCategoryReference});
+
+  final hazardCategoryName;
+  final hazardCategoryReference;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
