a7eb4dae53bdc2afb7365da7f25e208c296646a6
Ralph Lee <ralph.lee@live.ca>
2021-03-24 12:17:35 +0900

    Pass environmentReference to EnvironmentBox

--- lib/screens/environment.dart
+++ lib/screens/environment.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:risk_assessment_flutter/appbar.dart';
 import 'package:risk_assessment_flutter/constants.dart';
-import 'workplace_hazards.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../environment_box.dart';
 
 // Access a Cloud Firestore instance from your Activity
 final _firestore = FirebaseFirestore.instance;
 
 class Environment extends StatefulWidget {
-  Environment({this.industryID, this.environment});
+  Environment({this.industryReference});
 
-  final industryID;
-  final environment;
+  // final industryID;
+  final industryReference;
 
   @override
   _EnvironmentState createState() => _EnvironmentState();
@@ -37,7 +37,7 @@ class _EnvironmentState extends State<Environment> {
               ),
 
               StreamBuilder<QuerySnapshot>(
-                stream: widget.environment,
+                stream: widget.industryReference,
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     final environments = snapshot.data.docs;
@@ -47,9 +47,12 @@ class _EnvironmentState extends State<Environment> {
                     for (var environment in environments) {
                       // print(environment['environment_name']);
                       // print(environment.reference.collection('hazard_category').snapshots());
-                      environmentList.add(EnvironmentBox(
+                      environmentList.add(
+                        EnvironmentBox(
                           environmentName: environment['environment_name'],
-                          workplaceHazard: environment.reference.collection('hazard_category').snapshots()));
+                          environmentReference: environment.reference.collection('hazard_category').snapshots(),
+                        ),
+                      );
                     }
 
                     return Column(
@@ -64,7 +67,7 @@ class _EnvironmentState extends State<Environment> {
                 },
               )
 
-              // -------------------ORIGINAL STUFF-------------------
+              // -------------------ORIGINAL HARD-CODED STUFF-------------------
               // Column(
               //   mainAxisAlignment: MainAxisAlignment.start,
               //   crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -79,50 +82,6 @@ class _EnvironmentState extends State<Environment> {
               // -------------------ORIGINAL STUFF-------------------
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class EnvironmentBox extends StatelessWidget {
-  EnvironmentBox({this.environmentName, this.environmentID, this.workplaceHazard});
-
-  final String environmentName;
-  final String environmentID;
-  final Stream<QuerySnapshot> workplaceHazard;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: ElevatedButton(
-        child: Row(
-          children: [
-            Expanded(
-              child: Text(
-                environmentName,
-                style: kSubHeaderTextStyle,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            // return WorkplaceHazards(environmentID: environmentID, workplaceHazard: workplaceHazard);
-            return WorkplaceHazards(environmentID: environmentID);
-          }));
-        },
-        style: ElevatedButton.styleFrom(
-          primary: kAppBlue,
-          onPrimary: kAppLight,
-          elevation: 10,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          // padding: EdgeInsets.fromLTRB(80, 15, 80, 15),
-          padding: EdgeInsets.symmetric(vertical: 30),
         ),
       ),
     );
