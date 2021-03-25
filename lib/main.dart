de5550e7a5f95f78606af1d5a8e240980ba1eb8a
Ralph Lee <ralph.lee@live.ca>
2021-03-25 11:51:57 +0900

    Delete unused code and add Firebase initializer

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:risk_assessment_flutter/constants.dart';
 import 'screens/landing_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+bool USE_FIRESTORE_EMULATOR = false;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.settings =
+        const Settings(host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  }
+
   runApp(MyApp());
 }
 
@@ -11,18 +22,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'SAHARA',
-
+      title: 'SAHARA',
       theme: ThemeData(
         // primaryColor: Color(0XFFB5121B),
         primaryColor: kAppGreen,
         // primaryColor: Colors.grey[200],
         // scaffoldBackgroundColor: Colors.grey[100],
-
         scaffoldBackgroundColor: Color(0xFFDCD6DA),
       ),
       debugShowCheckedModeBanner: false,
-
       home: LandingPage(),
     );
   }
