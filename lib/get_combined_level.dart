7a0c5be5b4da428ec15b48295410e1edc013fe2d
Ralph Lee <ralph.lee@live.ca>
2021-04-02 17:11:58 +0900

    Add onLevelChange as a parameter in the constructor

--- lib/get_combined_level.dart
+++ lib/get_combined_level.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:risk_assessment_flutter/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'screens/result.dart';
 
 // Access a Cloud Firestore instance from your Activity
 final _firestore = FirebaseFirestore.instance;
 
 String combinedLevel;
+int combinedHigh;
 
-class GetRiskLevel extends StatelessWidget {
-  GetRiskLevel({this.totalHazard, this.totalMental, this.totalCombined, this.score, this.documentId});
+class GetCombinedLevel extends StatefulWidget {
+  GetCombinedLevel({this.score, this.documentId, this.containerColour, this.onLevelChange});
 
-  final totalHazard;
-  final totalMental;
-  final totalCombined;
   final score;
   final String documentId;
+  Color containerColour;
+  final Function(String) onLevelChange;
 
+  @override
+  _GetCombinedLevelState createState() => _GetCombinedLevelState();
+}
+
+class _GetCombinedLevelState extends State<GetCombinedLevel> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<DocumentSnapshot>(
-      future: _firestore.collection('risk_level').doc(documentId).get(),
+      future: _firestore.collection('risk_level').doc(widget.documentId).get(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return Text("Something went wrong");
@@ -27,21 +33,31 @@ class GetRiskLevel extends StatelessWidget {
 
         if (snapshot.hasData) {
           var data = snapshot.data.data();
+          combinedHigh = data['score_high'];
 
-          if (score >= data['score_high']) {
+          if (widget.score >= data['score_high']) {
             combinedLevel = data['level_high'];
+            widget.onLevelChange(combinedLevel);
+            // setState(() {
+            //   widget.containerColour = kResultHighColour;
+            // });
+
             return Text(
               combinedLevel,
               style: kCombinedResultTextStyle,
             );
-          } else if (score <= data['score_low']) {
+          } else if (widget.score <= data['score_low']) {
             combinedLevel = data['level_low'];
+            widget.onLevelChange(combinedLevel);
+
             return Text(
               combinedLevel,
               style: kCombinedResultTextStyle,
             );
           } else {
             combinedLevel = data['level_medium'];
+            widget.onLevelChange(combinedLevel);
+
             return Text(
               combinedLevel,
               style: kCombinedResultTextStyle,
