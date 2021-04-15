fe6f482227c7ab76d94cf0a19d7e0313af6491b9
Ralph Lee <ralph.lee@live.ca>
2021-04-15 22:44:14 +0900

    Add a setState to update the customHazardList with the passed data from custom_hazard file

--- lib/screens/hazard_category.dart
+++ lib/screens/hazard_category.dart
@@ -30,7 +30,13 @@ class _HazardCategoryState extends State<HazardCategory> {
 
   int totalScore = 0;
   static List<Hazard> _hazardList = [];
-  static List<Hazard> _customHazardList = [];
+  static List<Hazard> customHazardList1 = [];
+
+  // void updateInformation(String) {
+  //   setState(() {
+  //     customHazardList1 =
+  //   });
+  // }
 
   final _multiSelectKey = GlobalKey<FormFieldState>();
 
@@ -71,19 +77,128 @@ class _HazardCategoryState extends State<HazardCategory> {
                               final List<Widget> hazardCategoryList = [];
 
                               for (var hazardCategory in hazardCategories) {
-                                hazardCategoryList.add(
-                                  Container(
-                                    padding: EdgeInsets.all(10),
-                                    child: HazardDropdown(
-                                      hazardCategory: hazardCategory['hazard_category_name'],
-                                      hazardCategoryReference:
-                                          hazardCategory.reference.collection('hazard').snapshots(),
-                                      callback: (int total) {
-                                        totalHazardScore = total;
-                                      },
+                                if (hazardCategory['hazard_category_name'] != 'Custom') {
+                                  hazardCategoryList.add(
+                                    Container(
+                                      padding: EdgeInsets.all(10),
+                                      child: HazardDropdown(
+                                        hazardCategory: hazardCategory['hazard_category_name'],
+                                        hazardCategoryReference:
+                                            hazardCategory.reference.collection('hazard').snapshots(),
+                                        callback: (int total) {
+                                          totalHazardScore = total;
+                                        },
+                                      ),
                                     ),
-                                  ),
-                                );
+                                  );
+                                }
+                                // else {
+                                //   _hazardList = customHazardList1;
+                                //   return MultiSelectDialogField(
+                                //     items: _hazardList
+                                //         .map((hazard) => MultiSelectItem<Hazard>(hazard, hazard.hazardName))
+                                //         .toList(),
+                                //     title: Text(
+                                //       "Customm",
+                                //       textAlign: TextAlign.center,
+                                //     ),
+                                //     selectedColor: Colors.black,
+                                //     selectedItemsTextStyle: TextStyle(fontSize: 20),
+                                //     decoration: BoxDecoration(
+                                //       // color: kAppLight,
+                                //       color: Colors.white,
+                                //       borderRadius: BorderRadius.all(Radius.circular(10)),
+                                //       boxShadow: [
+                                //         BoxShadow(
+                                //           color: Colors.grey.withOpacity(0.6),
+                                //           spreadRadius: 4,
+                                //           blurRadius: 4,
+                                //           offset: Offset(0, 4), // changes position of shadow
+                                //         ),
+                                //       ],
+                                //     ),
+                                //     buttonText: Text(
+                                //       "Customm",
+                                //       style: kSubHeaderTextStyle,
+                                //     ),
+                                //     onConfirm: (results) {
+                                //       hazardScore[hazardCategory] = 0;
+                                //       var totalHazardScore = 0;
+                                //
+                                //       for (var i = 0; i < results.length; i++) {
+                                //         hazardScore[hazardCategory] += results[i].riskValue;
+                                //       }
+                                //
+                                //       hazardScore.forEach((key, value) {
+                                //         totalHazardScore += value;
+                                //       });
+                                //       // callback(totalHazardScore);
+                                //       print(totalHazardScore);
+                                //     },
+                                //   );
+                                // }
+
+                                // if (hazardCategory['hazard_category_name'] == 'Custom') {
+                                //   _hazardList = customHazardList1;
+                                //   return MultiSelectDialogField(
+                                //     items: _hazardList
+                                //         .map((hazard) => MultiSelectItem<Hazard>(hazard, hazard.hazardName))
+                                //         .toList(),
+                                //     title: Text(
+                                //       "Customm",
+                                //       textAlign: TextAlign.center,
+                                //     ),
+                                //     selectedColor: Colors.black,
+                                //     selectedItemsTextStyle: TextStyle(fontSize: 20),
+                                //     decoration: BoxDecoration(
+                                //       // color: kAppLight,
+                                //       color: Colors.white,
+                                //       borderRadius: BorderRadius.all(Radius.circular(10)),
+                                //       boxShadow: [
+                                //         BoxShadow(
+                                //           color: Colors.grey.withOpacity(0.6),
+                                //           spreadRadius: 4,
+                                //           blurRadius: 4,
+                                //           offset: Offset(0, 4), // changes position of shadow
+                                //         ),
+                                //       ],
+                                //     ),
+                                //     buttonText: Text(
+                                //       "Customm",
+                                //       style: kSubHeaderTextStyle,
+                                //     ),
+                                //     onConfirm: (results) {
+                                //       hazardScore[hazardCategory] = 0;
+                                //       var totalHazardScore = 0;
+                                //
+                                //       for (var i = 0; i < results.length; i++) {
+                                //         hazardScore[hazardCategory] += results[i].riskValue;
+                                //       }
+                                //
+                                //       hazardScore.forEach((key, value) {
+                                //         totalHazardScore += value;
+                                //       });
+                                //       // callback(totalHazardScore);
+                                //       print(totalHazardScore);
+                                //     },
+                                //   );
+                                // }
+                                // else {
+                                //   hazardCategoryList.add(
+                                //     Container(
+                                //       padding: EdgeInsets.all(10),
+                                //       child: HazardDropdown(
+                                //         hazardCategory: hazardCategory['hazard_category_name'],
+                                //         hazardCategoryReference:
+                                //             hazardCategory.reference.collection('hazard').snapshots(),
+                                //         callback: (int total) {
+                                //           totalHazardScore = total;
+                                //         },
+                                //       ),
+                                //     ),
+                                //   );
+                                // }
+
                               }
 
                               return Column(
@@ -96,6 +211,8 @@ class _HazardCategoryState extends State<HazardCategory> {
                             }
                           },
                         ),
+
+                        // FutureBuilder to retrieve the threshold data from backend for result colour
                         FutureBuilder<DocumentSnapshot>(
                           future: _firestore.collection('risk_level').doc(documentId).get(),
                           builder: (context, snapshot) {
@@ -115,7 +232,11 @@ class _HazardCategoryState extends State<HazardCategory> {
 
                             return Text("");
                           },
-                        )
+                        ),
+                        // Container(
+                        //   padding: EdgeInsets.all(10),
+                        //   child: HazardDropdown(hazardCategory: "Test"),
+                        // ),
                       ],
                     ),
                   ],
@@ -123,9 +244,41 @@ class _HazardCategoryState extends State<HazardCategory> {
               ],
             ),
           ),
-          NextButton(
-            buttonText: 'Add Custom Hazard',
-            nextWidget: CustomHazard(),
+          // NextButton(
+          //   buttonText: 'Add Custom Hazard',
+          //   nextWidget: CustomHazard(),
+          // ),
+          Container(
+            padding: EdgeInsets.fromLTRB(50, 0, 50, 50),
+            width: 400,
+            child: ElevatedButton(
+              child: Text(
+                "Add Custom Hazard",
+                style: TextStyle(fontSize: 20, fontFamily: 'Raleway'),
+              ),
+              onPressed: () async {
+                final result = await Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return CustomHazard();
+                }));
+
+                ScaffoldMessenger.of(context)
+                  ..removeCurrentSnackBar()
+                  ..showSnackBar(SnackBar(content: Text("$result")));
+
+                setState(() {
+                  customHazardList1.add(Hazard(hazardName: '$result'));
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                primary: kAppBlue,
+                // onPrimary: kAppLight,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+              ),
+            ),
           ),
           Container(
             padding: EdgeInsets.fromLTRB(50, 0, 50, 50),
@@ -160,7 +313,6 @@ class _HazardCategoryState extends State<HazardCategory> {
     );
   }
 
-  // Hazard Dropdown widget from package
   HazardDropdown({hazardCategory, hazardCategoryReference, callback}) {
     return StreamBuilder<QuerySnapshot>(
       stream: hazardCategoryReference,
@@ -169,6 +321,7 @@ class _HazardCategoryState extends State<HazardCategory> {
           final hazards = snapshot.data.docs;
 
           _hazardList = [];
+
           for (var hazard in hazards) {
             _hazardList.add(
               Hazard(
@@ -232,7 +385,7 @@ class _HazardCategoryState extends State<HazardCategory> {
 
 // Hazard constructor
 class Hazard {
-  Hazard({this.riskValue, this.hazardName});
+  Hazard({this.hazardName, this.riskValue});
 
   final String hazardName;
   final int riskValue;
